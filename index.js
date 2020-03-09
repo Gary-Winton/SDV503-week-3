8bec1c43e831e7f5177d2b1a112deefcab526762
Gzary Winton <Gary-Winton@live.nmit.ac.nz>
2020-03-09 14:04:13 +1300

    increments

--- index.js
+++ index.js
@@ -10,3 +10,33 @@ console.log(x ** y)
 console.log(x + ++y)
 
 // increment before add the number straight away when increment after takes an extra step before adding
+
+x = 10
+y = x 
+
+console.log ("pre - increment")
+
+y = ++x
+console.log(x)
+console.log(y)
+
+console.log("post - increment")
+
+y = x++
+console.log(x)
+console.log(y)
+
+x = 10
+y = x 
+
+console.log ("pre - increment")
+
+y = --x
+console.log(x)
+console.log(y)
+
+console.log("post - increment")
+
+y = x--
+console.log(x)
+console.log(y)
