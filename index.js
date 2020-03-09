8b16e2d923c3f385196ac6e6a67070801f4351d1
Gzary Winton <Gary-Winton@live.nmit.ac.nz>
2020-03-09 13:52:38 +1300

    single line comment

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ console.log(x / y)
 console.log(x % y)
 console.log(x ** y)
 console.log(x + ++y)
+
+// increment before add the number straight away when increment after takes an extra step before adding
